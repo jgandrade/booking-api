f3e34fe591e45078a30557d024f0d198d963b27c
jgandrade1 <johnglenn.andrade@gmail.com>
2022-09-06 15:15:36 +0800

    Finish Archive Course

--- controllers/courseControllers.js
+++ controllers/courseControllers.js
@@ -11,7 +11,6 @@ module.exports.addCourse = (req, res) => {
             courseSlots: req.body.courseSlots
         })
 
-        
         return newCourse.save()
             .then(result => res.send("Successfully Saved"))
             .catch(err => res.send(err.message));
@@ -19,3 +18,67 @@ module.exports.addCourse = (req, res) => {
         res.send({ message: "You are not authorize to add a course" });
     }
 }
+
+module.exports.getAllCourse = (req, res) => {
+    let data = auth.decode(req.headers.authorization);
+    if (data.isAdmin) {
+        Course.find({})
+            .then(result => {
+                res.send(result);
+            }).catch(err => res.send("No courses found!"));
+    } else {
+        return res.send("Not allowed");
+    }
+}
+
+module.exports.getAllActiveCourse = (req, res) => {
+    Course.find({ isCourseActive: true })
+        .then(result => {
+            if (result) return res.send(result);
+
+        }).catch(err => res.send("No courses found!"));
+}
+
+module.exports.getSpecificCourse = (req, res) => {
+    Course.findById({ _id: req.params.id })
+        .then(result => {
+            return res.send(result);
+        }).catch(err => res.send("No such id like that!"));
+}
+
+module.exports.updateCourse = (req, res) => {
+
+    let data = auth.decode(req.headers.authorization);
+    if (data.isAdmin) {
+        let updateCourse = {
+            courseName: req.body.courseName,
+            courseDescription: req.body.courseDescription,
+            coursePrice: req.body.coursePrice,
+            courseSlots: req.body.courseSlots
+        }
+        return Course.findByIdAndUpdate(req.params.id, updateCourse, { new: true })
+            .then(result => {
+                return res.send(result);
+            }).catch(err => res.send("No such id like that!"));
+    } else {
+        return res.send("Not allowed");
+    }
+
+}
+
+module.exports.archiveCourse = (req, res) => {
+    let data = auth.decode(req.headers.authorization);
+    if (data) {
+        let updateIsActiveField = {
+            isCourseActive: req.body.isCourseActive
+        }
+        Course.findByIdAndUpdate(req.params.id, updateIsActiveField)
+            .then(result => {
+                return res.send(result);
+            }).catch(err => res.send("No such id like that!"));
+
+    } else {
+        return res.send("Your token is not authorized.")
+    }
+}
+
